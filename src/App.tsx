239678cda20cba9516bc2bd272a0444edb62cbea
Ariel Eitner <105615462+Ariel-Eitner@users.noreply.github.com>
2024-02-20 12:39:57 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { LoginButton } from "./components/NavBar/LoginButton";
-import { useEffect } from "react";
-import { LogoutButton } from "./components/NavBar/LogoutButton";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+// import { LoginButton } from "./components/NavBar/LoginButton";
+// import { useEffect } from "react";
+// import { LogoutButton } from "./components/NavBar/LogoutButton";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
 import { MainNavbar } from "./components/NavBar/NavBar";
 import { RegistrationForm } from "./components/registracion/ResgistrationForm";
 
